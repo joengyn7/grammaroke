ab3f01a96bc0e01c5b60c350d47cecdf69d54a02
joengyn7 <joe.ngyn7@gmail.com>
2020-11-29 18:39:12 -0500

    1st draft header

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <div>
+        <span>命に嫌われている</span>
+      </div>
+      <div>
+        <button>
+          <img></img>
+          <span>Add Text</span>
+        </button>
+        <span>/</span>
+        <button>
+          <img></img>
+          <span>Styles Guides</span>
+        </button>
+      </div>
       </header>
     </div>
   );
