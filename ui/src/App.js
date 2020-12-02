213ebd51bcda1bcf53d096ddd568e36f69d59d8b
joengyn7 <joe.ngyn7@gmail.com>
2020-12-01 21:11:33 -0500

    header & toolbar laid out

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,31 @@
 import "./app.scss"
-
 const songTitle = '命に嫌われている'
 
 function App() {
   return (
     <div className="App">
+
+      {/* Header */}
       <header>
-        <div>
-          <text>
-            <a href="">Home</a>
-            <span>/</span>
-            <a href="">My Songs</a>
-          </text>
+        <div className="nav">
+          <a href="#homepage">Home</a>
+          <span>/</span>
+          <a href="#mysongs">My Songs</a>
         </div>
-        <div>
+        <div className="song-title">
           <div>{songTitle}</div>
         </div>
       </header>
+      {/* End Header */}
+
+      {/* Toolbar */}
+      <div className="toolbar">
+        <button>1<span>Add Text</span></button>
+        <button>2<span>Style Guides</span></button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+      {/* End Toolbar */}
     </div>
   );
 }
