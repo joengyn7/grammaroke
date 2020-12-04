147c8f6231d6f34217081434b82ffc2648f85319
joengyn7 <joe.ngyn7@gmail.com>
2020-12-04 17:29:11 -0500

    text input into stanzas

--- ui/src/components/views/addSongText/AddSongText.js
+++ ui/src/components/views/addSongText/AddSongText.js
@@ -1,16 +1,46 @@
+import React from 'react';
 import Toolbar from "../../toolbar/Toolbar"
 import empty from "../../../assets/empty-state.png"
 import "./addSongText.scss"
 
-const songTitle = '命に嫌われている'
-
 function AddSongText() {
+  const [didClick, setDidClick] = React.useState(false)
+  const [text, setText] = React.useState("")
+
+  const clickedSave = () => {
+    const sanitized = text.split("\n").map(s => s.trim())
+    const stanzas = []
+    let newStanza = true;
+    let currentStanza;
+    sanitized.forEach(s => {
+      if (s === "") {
+        newStanza = true;
+      } 
+      else {
+        if (newStanza === true) {
+          newStanza = false;
+          stanzas.push([])
+          currentStanza = stanzas[stanzas.length - 1]
+        }
+        currentStanza.push(s)
+      }
+    })
+    console.log(stanzas)
+  }
+
   return (
     <div className="AddSongText">
         <Toolbar />
-        <div className="body">
-            <img src={empty} alt="empty"></img>
-            <button>Add Text</button>
+        <div className="body">       
+            {didClick 
+              ? <>
+                  <textarea value={text} onChange={e => {setText(e.target.value)}}/>
+                  <button onClick={clickedSave}>Saved</button> 
+                </>
+              : <>
+                  <img src={empty} alt="empty"></img>
+                  <button onClick={() => setDidClick(true)}>Add Text</button>
+                </>}
         </div>
     </div>
   );
