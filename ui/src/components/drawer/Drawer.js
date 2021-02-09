05b3eb2a2d69408dafc257dab7e2d711bac28348
Joseph Nguyen <49961895+joengyn7@users.noreply.github.com>
2021-02-08 22:20:03 -0500

    Dev (#3)
    * added side drawer
    
    * finalized repsonsive header and sidebar

--- ui/src/components/drawer/Drawer.js
+++ ui/src/components/drawer/Drawer.js
@@ -6,10 +6,12 @@ function Drawer({drawerOpen, setDrawerOpen}) {
     return (
         <div className="drawer-container" style={{ transform: drawerOpen ? undefined : 'translatex(-100%)' }}>
             <button class="material-icons" onClick={() => setDrawerOpen(false)}>close</button>
-            <div><Link to="/">Home</Link></div>
-            <div><Link to="/mysongs">My Songs</Link></div>
-            <div><a>Profile</a></div>
-            <div><a>Settings</a></div>
+            <div className="links">
+                <div><Link to="/">Home</Link></div>
+                <div><Link to="/mysongs">My Songs</Link></div>
+                <div><a>Profile</a></div>
+                <div><a>Settings</a></div>
+            </div>
         </div>
     );
   }
