e2c34c70dc2e65e1f10af0a422c1c4f2a3cfbc6f
joengyn7 <joe.ngyn7@gmail.com>
2021-02-10 22:08:02 -0500

    added mobile homepage

--- ui/src/components/views/home/Home.js
+++ ui/src/components/views/home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
-
+import warning from "../../../assets/warning.png"
+import "./home.scss"
 
 function Home() {
     return (
         <div className="home">
-            <h1>Welcome to Grammaroke.</h1>
-            <p>This is the personal song book of Joe N. A tool used in the study of Japanese through singing along to music.</p>
+            <h1>Welcome <br />to Grammaroke.</h1>
+            <div className="bullets">
+                <div>
+                    <span class="material-icons first">fiber_manual_record</span>
+                    <span>Study and sing along to your favorite songs.</span>
+                </div>
+                <div>
+                    <span class="material-icons second">fiber_manual_record</span>
+                    <span>Add color coded notes. Use for translation, grammar, particles, conjugatin rules.</span>
+                </div>
+                <div>
+                    <span class="material-icons third">fiber_manual_record</span>
+                    <span>Apply your knowledge immediately. Language learning made fun.</span>
+                </div>
+                <div>
+                    <span class="material-icons fourth">fiber_manual_record</span>
+                    <span>Check out the full Case Study <a href="https://kendojo.myportfolio.com/grammaroke">here.</a></span>
+                </div>
+            </div>
+            <div className="warning">
+                <img src={warning} alt="empty"></img>
+                <span>This site is still under development.<br />For updates, please visit the Github <a href="https://github.com/joengyn7/grammaroke">here</a>.</span>
+            </div>
         </div>
     )
 };
