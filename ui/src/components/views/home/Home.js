e2ce19cd1cce62ecc4c320ab03375b87c68cfb58
joengyn7 <joe.ngyn7@gmail.com>
2021-02-18 22:03:57 -0500

    changed home layout to grid

--- ui/src/components/views/home/Home.js
+++ ui/src/components/views/home/Home.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import warning from "../../../assets/warning.png"
+import video from '../../../assets/demo.mov'
 import "./home.scss"
 
 function Home() {
     return (
-        <div className="container">
-            <div className="home">
+        <div className="home">
+            <div className="text">
                 <h1>Welcome <br />to Grammaroke.</h1>
-                <div className="bullets">
-                    <div>
-                        <span class="material-icons first">fiber_manual_record</span>
-                        <span>Study and sing along to your favorite songs.</span>
-                    </div>
-                    <div>
-                        <span class="material-icons second">fiber_manual_record</span>
-                        <span>Add color coded notes. Use for translation, grammar, particles, conjugatin rules.</span>
-                    </div>
-                    <div>
-                        <span class="material-icons third">fiber_manual_record</span>
-                        <span>Apply your knowledge immediately. Language learning made fun.</span>
-                    </div>
-                    <div>
-                        <span class="material-icons fourth">fiber_manual_record</span>
-                        <span>Check out the full Case Study <a href="https://kendojo.myportfolio.com/grammaroke">here.</a></span>
-                    </div>
+                <div>
+                    <span class="material-icons first">fiber_manual_record</span>
+                    <span>Study and sing along to your favorite songs.</span>
                 </div>
-                <div className="warning">
-                    <img src={warning} alt="empty"></img>
-                    <span>This site is still under development.<br />For updates, please visit the Github <a href="https://github.com/joengyn7/grammaroke">here</a>.</span>
+                <div>
+                    <span class="material-icons second">fiber_manual_record</span>
+                    <span>Add color coded notes. Use for translation, grammar, particles, conjugation rules.</span>
+                </div>
+                <div>
+                    <span class="material-icons third">fiber_manual_record</span>
+                    <span>Apply your knowledge immediately. Language learning made fun.</span>
+                </div>
+                <div>
+                    <span class="material-icons fourth">fiber_manual_record</span>
+                    <span>Check out the full Case Study<a href="https://kendojo.myportfolio.com/grammaroke">here.</a></span>
                 </div>
             </div>
             <div className="lyrics-view">
@@ -42,8 +36,15 @@ function Home() {
                     </ul>
                 </div>
             </div>
+            <div className="video">
+                <video width="100%" height="auto" controls >  
+                    <source src={video} type="video/mp4"/>
+                </video>
+            </div>
         </div>
     )
 };
 
 export default Home;
+
+    
