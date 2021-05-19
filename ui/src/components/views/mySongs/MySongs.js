480f26738568b7ed3e3f54e35f22a41e257b2670
joengyn7 <joe.ngyn7@gmail.com>
2021-05-19 14:08:07 -0400

    tablet responsive done

--- ui/src/components/views/mySongs/MySongs.js
+++ ui/src/components/views/mySongs/MySongs.js
@@ -6,38 +6,60 @@ import view_list from '../../../assets/view_list.svg'
 import one from '../../../assets/one.jpeg'
 import two from '../../../assets/two.jpeg'
 import three from '../../../assets/three.png'
+import regal1997 from '../../../assets/1997.jpg'
+import 気になるあの子 from '../../../assets/気になるあの子.jpg'
+import 命に嫌われている from '../../../assets/命に嫌われている.jpg'
+import Elma from '../../../assets/elma.jpeg'
+import CoffeeTime from '../../../assets/CoffeeTime.jpeg'
+import SeasideSoil from '../../../assets/SeasideSoil.webp'
+
 
 // TODO Connect API to use real data
 const fakeData = [
     {
         imgSrc: one,
-        songTitle: "One One One",
-        artist: "Kano Kano Kano"
+        songTitle: "One",
+        artist: "Kano - 鹿乃"
     },
     {
         imgSrc: two,
-        songTitle: "Two Two Two",
-        artist: "Kano"
+        songTitle: "Two",
+        artist: "Kano - 鹿乃"
     },
     {
         imgSrc: three,
-        songTitle: "Three Three Three",
-        artist: "Kano"
+        songTitle: "Three",
+        artist: "Kano - 鹿乃"
     },
     {
-        imgSrc: one,
-        songTitle: "One One One",
-        artist: "Kano Kano Kano"
+        imgSrc: regal1997,
+        songTitle: "1997",
+        artist: "Regal Lily"
     },
     {
-        imgSrc: two,
-        songTitle: "Two Two Two",
-        artist: "Kano"
+        imgSrc: 気になるあの子,
+        songTitle: "気になるあの子",
+        artist: "相対性理論"
     },
     {
-        imgSrc: three,
-        songTitle: "Three Three Three",
-        artist: "Kano"
+        imgSrc: 命に嫌われている,
+        songTitle: "命に嫌われている",
+        artist: "カンザキイオリ"
+    },
+    {
+        imgSrc: Elma,
+        songTitle: "Elma",
+        artist: "Yorushika"
+    },
+    {
+        imgSrc: CoffeeTime,
+        songTitle: "Coffee Time",
+        artist: "Yorushika"
+    },
+    {
+        imgSrc: SeasideSoil,
+        songTitle: "Seaside Soil",
+        artist: "Orangestar"
     }]
 
 function MySongs() {
